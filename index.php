f6904bc827fbd4af569338775929f574360adc31
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-03-01 12:28:56 +0100

    bonus 1: parking filter

--- index.php
+++ index.php
@@ -1,44 +1,60 @@
 <?php
 
-    $hotels = [
+$hotels = [
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
 
-    ];
+];
+
+$filteredHotels = $hotels;
+
+if ( isset($_GET['parking']) && $_GET['parking'] == '1' ) {
+    $hotelWithParking = [];
+
+    foreach($filteredHotels as $element){
+        if ($element['parking']) {
+            $hotelWithParking[] = $element;
+        }
+    }
+
+    $filteredHotels = $hotelWithParking;
+}
+
+
 
 ?>
 
@@ -56,6 +72,17 @@
     <h1 class="text-center mt-5">Hotels</h1>
 
     <div class="container mt-5">
+        <form action="index.php" method="get" class="w-25">
+            <select name="parking" class="form-select" >
+                <option selected <?= isset($_GET['parking']) && $_GET['parking'] == '1' ? '' : 'selected' ?> >Parking</option>
+                <option value="1" <?= isset($_GET['parking']) && $_GET['parking'] == '1' ? 'selected' : '' ?> >Yes</option>
+            </select>
+            <button class="btn btn-primary mt-2" type="submit">Search</button>
+        </form>
+    </div>
+
+
+    <div class="container mt-5">
         <table class="table">
         <thead>
             <tr>
@@ -67,7 +94,10 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach($hotels as $hotel): ?>
+
+            
+
+            <?php foreach($filteredHotels as $hotel): ?>
                 <tr>
                     <td> <?= $hotel['name'] ?> </td>
                     <td> <?= $hotel['description'] ?> </td>
